c1d4e4755b237454e10da7b17f2137fec4cf6a74
hirokiss <astraea6514@icloud.com>
2021-09-26 23:38:33 +0900

    createとviewファイルの編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,12 +1,20 @@
 class PrototypesController < ApplicationController
   
   def index
+    @prototypes = Prototype.all
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -19,5 +27,11 @@ class PrototypesController < ApplicationController
   end
   
   def show
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
